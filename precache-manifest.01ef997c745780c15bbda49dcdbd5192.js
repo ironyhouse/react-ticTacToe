c2b0371a15239cd69268f43da9705d6162a7924b
ironylait <ironylait@mail.ru>
2020-01-06 00:30:49 +0300

    Updates

---
+++ precache-manifest.01ef997c745780c15bbda49dcdbd5192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc3425b15fe5ad8103ca8dc8dab0bba",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "217001b1dfccca0be19f",
+    "url": "/tic-tac-toe/static/css/main.f60c9681.chunk.css"
+  },
+  {
+    "revision": "acac649ffdd08f354247",
+    "url": "/tic-tac-toe/static/js/2.40d2457e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.40d2457e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "217001b1dfccca0be19f",
+    "url": "/tic-tac-toe/static/js/main.bb70a741.chunk.js"
+  },
+  {
+    "revision": "84d27177a9ac2da25509",
+    "url": "/tic-tac-toe/static/js/runtime-main.8be6601f.js"
+  }
+]);
